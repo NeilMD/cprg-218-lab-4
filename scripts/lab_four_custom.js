d2133db69a735c2cf84816b2e7410847fc272368
Neil Capistrano <neil.capistrano2007.ca@gmail.com>
2024-03-04 18:35:53 -0700

    feat: added googe fonts and comments

--- scripts/lab_four_custom.js
+++ scripts/lab_four_custom.js
@@ -1,3 +1,15 @@
+/*
+    Accordion Javascript
+
+    Add event listener on button
+    Everytime button is click, the following are change:
+    - Toggle active class in the button
+    - Toggle the active-panel class in the div next to the button
+    - Toggle the arrow button of the idiomatic text tag inside the button
+
+*/
+
+
 let accordions = document.querySelectorAll("button.accordion");
 
 accordions.forEach((acco) => {
@@ -8,5 +20,13 @@ accordions.forEach((acco) => {
     acco.getElementsByClassName("fa-solid")[0].classList.toggle("fa-chevron-down")
   };
 });
+
+
+/*
+    Copyright Javascript
+
+    Set the text of the span to current year
+    
+*/
 
 document.getElementById("footer-year").textContent = new Date().getFullYear();
